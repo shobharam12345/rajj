4ca6c2d5fb312c9a20e641e8e7c415de3dbd1835
SHOBHARAM Waskel <161594556+shobharam12345@users.noreply.github.com>
2024-06-20 15:29:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,534 @@
+// name='shobharam'
+
+
+// sectio'bia-4'
+
+
+
+
+
+
+// opratores
+
+  
+// Nullish
+//  what is sig of ncc??
+// comparesion with  operators
+
+
+
+// console.log(NULL??2)
+
+
+// console.log(false||''||55)
+// console.log(false??''??55)
+
+
+
+// let student={ 
+// Name:'lokendra',
+// Selection:{
+
+//     batch:4
+
+// }
+
+// }
+
+
+// console.log(student.Selection.group??'not group  found')
+
+
+// let marks=100
+
+// if(marks<=33){
+//     console.log(not eligible)
+//       else(marks<=33)
+//       console.log(pass) 
+      
+    
+
+
+
+// let marks=33
+
+
+
+
+
+
+// let lightOn  ='red'
+
+// if(lightOn=='red ')
+
+
+//     let  firstdivision=60
+//     marks>54?console.log('first')
+//     // marks<=54&&marks
+//         console.log(firstdivision) 
+
+
+
+
+//     let number=0
+
+//     number>0?console.log('positive'):
+//     number<=0?console.log('negative'):
+// console.log('zero')
+
+
+
+// switch case//
+
+
+
+
+//  (variable){
+//     case value:
+//         code execution
+//         break;
+//         case value:
+    
+//             break
+//             default:
+//             code execution
+            
+//             let name='shobharam'
+            
+//             switch(name){
+//                 case 'shobharam':
+//                     console.log('yes this is my name')
+//                     break;
+//                     dswitchefault:
+//                     console.log('this is not my name')
+//             }
+// // }
+// let marks=64
+// switch(marks){
+//     case (marks>60):
+//         console.log ('first division')
+//         break;
+//         case (marks<=60&&marks>=33):
+//             console.log('second')
+//             break;
+//             default:
+//                 console.log('third division')
+// }
+
+
+// let  age =21
+// switch(true){
+
+//   case  (age >18):
+// console.log('you are eligible for vote')
+// break;
+// case (age):
+//     console.log('you are not eligible for vote')
+//     break;
+// }
+
+// OBJECT//
+
+// let sarthakDetails={
+//     name:'Sarthak Dhoke'
+// }
+// let object={
+//     name:'shobharam',
+//     section:'bia4'
+//     variable type
+// }
+
+
+
+// CRUD//
+
+// let obj={}
+// console.log(obj)
+// obj.name='Shobharam'
+// console.log(obj)
+
+// obj.section='bia 4'
+// obj.fathersname='mangilal'
+// obj.mothersName='ramkor bai'
+// console.log(obj)//
+
+
+// function ninjaNumber(n){
+//         let count=1
+//     for (let i=1;i=n;i++){
+//        let str=''
+//        for  (let j=1;j<=(n-1);j++){
+//         str=str+' '
+//     }
+//         for (let k=1;k<=1;k++){
+
+//         str=str+count
+//     }
+//         count=(count%9)+1
+//         console.log(str)
+//     }
+// }
+
+// let obj1={}
+// console.log(obj1)
+// let obj2=new Object();
+// console.log(obj2)
+
+
+// //key : 'flaskdjf'
+
+// obj1.section='bia4'
+// obj1.center='ddugky'
+// console.log(obj1) /
+
+
+// // 
+// let car={
+//     name:'Bolero',
+//     engineCapacity:'800cc'
+//     driveMode:'auto'
+// }
+
+
+// let ddugky={}
+
+// ddugky.fundedBy='Government'
+// ddugky.runBy='JSMIES'
+// ddugky.totalRunningBatch=3
+// ddugky.totalStudent=65
+// console.log(ddugky)
+
+
+
+// let computer={
+//     father:'charls',
+//     createdIn:1946,
+//     part:['cpu','monitor',"mouse"],
+//     type:3
+// }
+// console.log(computer)
+// computer.type=4
+// computer.father='shobharam'
+// console.log(computer)
+
+// console.log(computer)
+// delete computer.father
+
+// // console.log(computer)
+
+// let details={
+//     name:'shobharam'
+//     Section:'bia 4',
+//     skills:['html','css'],
+//     village:'petlawad', 
+//     name:'Mr mangilal waskel',
+//     occupation:'farmer',
+//     age:'60'
+//     console.log(fatherdetails.name)
+
+    
+
+//   function reversePyramid(n){
+//   for (let i=1;i<=n;i++){
+//     for (let j=0; j<i; j++){
+//         str+= " ";
+//     }
+// for (let k =)
+//     // console.log('*')
+// }
+
+// }
+
+//    reversePyramid(5)
+
+
+
+
+
+
+
+//    let n = 5; // you can take input from prompt or change the value
+// let string = "";
+// // External loop
+// for (let i = 0; i < n; i++) {
+//   // printing spaces
+//   for (let j = 0; j < i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < 2 * (n-i) - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// function printPattern(n){
+//     for(let i=1; i<=n;i++){
+//         console.log('')
+//         let str=' '
+//         for(let j=1; j<=(n-i+1); j++){
+//             str=str+'*'
+
+//         }
+//         console.log(str)
+//     }
+// }
+// printPattern(5)
+
+
+
+// function reverse(n){
+//     for(let i=1; i<=n; i++){
+//         let str=' '
+//         for(let j=1; j<=1; j++){
+//          str=str+'0'
+//         }for(let k=1; k<=n-1; k++){
+//             str=str+'x-'
+//         }
+
+//         console.log(str)
+//     }
+// }
+// reverse(4)
+//  console.log(str)
+// console.log(reverse)   
+
+// let movie={
+//     img:''
+//     tittle:''
+//     ott:''
+//     price:''
+//     currency:'rs'
+//     description:'its a nice movie'
+//     rating:4.5
+// }
+
+
+// function hollowSquare(n){
+
+// for (let i=1;i<=n;i++){
+//     let str=' '
+//     for(let j=1;j<=n;j++){
+//         if (i ==1 || i == n|| j ==1 || j==n) {
+//          str=str+'w'
+//         }
+//         else {  
+//             str=str+' '
+        
+//         }
+        
+//     }
+//     console.log(str)
+// }
+// }
+
+// hollowSquare(5)
+
+
+// function hollowSquare(b){
+
+//     for ( i=1;i<=n;i++){
+//         let str=''
+//         for (j=1;j<=n;j++){
+//             if ((i ==1) && (i == n)|| (j ==1 )&& (j==n))
+//             (i == n)&& (j ==1 )||(j ==1 )&& (j==n)
+//             {
+//              str=str+'M'
+//             }
+//             else { 
+//                 str=str+' 0 '
+            
+//             }
+            
+//         }
+//         console.log(str)
+//     }
+//     }
+    
+//     hollowSquare(4)
+
+
+// let obj={
+//     name: "shobharam",
+//     let: 'waskel'
+    
+// }
+// console.log(obj.name .let)
+
+
+// let array =[2,4,5,6,10]
+
+// let sum=0
+// for (let i=0;i<array.length;i++){
+//     sum=sum+array[i]
+// }
+// console.log(sum)
+
+
+// let array =[[1,2,3], [4,5,6],[7,8,9]]
+// let sum=0;
+// for (let i=0;i<array.length;i++){
+//     //i=0/1/2/3
+// for (let j=0;j<array[i].length;j++){
+//     //j=0/1/2/3
+//     sum=sum+array[i][j]//1+2+3+4+5+6+7+8+9
+//     }
+     
+// }
+    
+// console.log(sum)
+
+
+
+// function sumofArray(arr){
+//     let sum=0
+//     for (let i=0;i<arr.length;i++){
+
+//         for ( let j=0; j<arr[i].length; j++){
+        
+//         if ((i==0&&j==0)||(i==1&&j==1)||(i==arr.length-1&&j==arr.length-1)){
+//             sum=sum+arr[i][j]
+//         }
+//     }
+//     }
+// console.log(sum)
+// }
+
+// let array =[[1,2,3], [4,5,6],[7,8,9]]
+// sumofArray(array)
+
+
+// function printPattern(n){
+//     for(let i=1; i<=n;i++){
+//         // console.log('')
+//         let str=''
+//         for(let j=1; j<=i;j++){
+//            str=str+'love '
+//         }
+           
+//            console.log(str)
+//         }
+//         for(let k=1; k<=n-1; k++){
+//             let str=''
+//             for (l=1; l<=n-k;l++){
+//                 str=str+'love '
+//             }
+//             console.log(str)
+//         }
+//     }
+
+// printPattern(15)
+
+// second method //
+
+// function pattern (n){
+//     let totalrows=(n*2)-1
+//     let count=1
+//     for(let i=1;i<=totalrows;i++){
+//         let str=''
+//         for(let j=1;j<=count;j++){
+            
+//          str=str+'x'
+//         }
+//             if(i<n){
+//                 count++
+//             }else{count--}
+//              console.log(str)
+//         }
+
+//         }
+// printPattern(5)
+
+//
+// function printPattern(n){
+//     for(let i=1; i<=n;i++){
+//         // console.log('')
+//         let str=''
+//         for(let j=1; j<=i;j++){
+//            str=str+'love '
+//         }
+           
+//            console.log(str)
+//         }
+//         for(let k=1; k<=n-1; k++){
+//             let str=''
+//             for (l=1; l<=n-k;l++){
+//                 str=str+'love '
+//             }
+//             console.log(str)
+//         }
+//     }
+
+// printPattern(15)
+
+
+// f
+
+
+
+// function pattern(n){
+//     let space1=n
+//     for(let i=1;i<=n-1;i++){
+//         let str=''
+//         for(let j=1;j<=i;j++){
+//             str=str+'x'
+//         }
+//         for(let k=1;k<=space1;k++){
+//             str=str+' ' 
+//         }
+//         space1=space1-2
+//         for(let l=1;l<=i;l++){
+//             str=str+'x'
+//         }
+//         console.log(str)
+//     }
+    
+// }
+// printPatter(0)
+
+//key in object//
+// for in loop //
+
+
+
+// let  adiObject={}
+
+//     adiObject.name='aditya'
+//     // console.log(adiObject)
+//     adiObject.age='22'
+//     adiObject['gender']='male'
+//     console.log(adiObject)
+//     adiObject.gender
+//     adiObject.age
+
+//     console.log(adiObject.name)
+
+
+// tofixed
+
+
+
+// let a=23.236
+// console.log(a.tofixed(1))
+
+// let a=' ahghg'
+// console.log(isNaN(a))
+
+
+//isFinite
+
+
+// let a =Infinity
+// console.log(isFinite(a))
+
+
+// let a='23.23a734'
+// console.log(Math.floor())
+
+
+
+
+
+
+console.log(typeof'hello')
